c9073cfd4074e5071069cb5d0b933f2e4f4ecfad
Sanking-Lee <1310179113@qq.com>
2019-03-17 00:13:06 +0800

    plus one

---
+++ src/com/company/PlusOne.java
@@ -0,0 +1,45 @@
+package com.company;
+
+/**
+ * Author: Sean
+ * Date: Created In 0:03 2019/3/17
+ * Title: plusone
+ * Description:
+ * Version: 0.1
+ * Update History:
+ * [Date][Version][Author] What has been done;
+ */
+
+public class PlusOne {
+    public static void plusOne() {
+        int[] digits = {9};
+        int carry = 1;
+        int i = digits.length - 1;
+        while (i >= 0) {
+            int sum = digits[i] + carry;
+            if (sum < 10) {
+                carry = 0;
+                digits[i] = sum;
+                break;
+            } else {
+                carry = 1;
+                digits[i] = sum % 10;
+            }
+            i--;
+        }
+        if (carry == 1) {
+            int[] res = new int[digits.length + 1];
+            System.arraycopy(digits, 0, res, 1, digits.length);
+            res[0] = 1;
+            print(res);
+        } else {
+            print(digits);
+        }
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
