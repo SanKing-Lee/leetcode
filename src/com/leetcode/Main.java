79512ed50eec08030f2eb7581890b04459538049
Sanking-Lee <1310179113@qq.com>
2019-03-17 00:15:36 +0800

    plus one

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,9 +1,11 @@
-package com.company;
+package com.leetcode;
+
+import com.leetcode.Array.PlusOne;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        //PlusOne.plusOne();
+        PlusOne.plusOne();
     }
 }
